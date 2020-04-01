2cb1880f4f2fe1ae8986bd2978849d68dd41b96f
Andriy <andriy.shakh@epitech.eu>
2020-04-01 14:36:36 +0200

    Request ✅foreach ( as  => ) {

--- Core/Request.php
+++ Core/Request.php
@@ -10,5 +10,10 @@ class Request
       $newvalue = htmlspecialchars(stripslashes(trim($value)));
       $_POST[$key] = $newvalue;
     }
+    foreach ($_GET as $key => $value) {
+      // $newvalue = trim($value);
+      $newvalue = htmlspecialchars(stripslashes(trim($value)));
+      $_GET[$key] = $newvalue;
+    }
   }
 }
